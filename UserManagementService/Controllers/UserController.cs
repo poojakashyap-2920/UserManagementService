6ac830649e653d113a67ea0f4b66917ecdebecf0
poojakashyap-2920 <pooja337026@gmail.com>
2024-04-18 18:44:30 +0530

    usermanagementse  added

---
+++ UserManagementService/Controllers/UserController.cs
@@ -0,0 +1,158 @@
+﻿using BusinessLayer.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer.Entity;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Threading.Tasks;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBl _userBl;
+
+        public UserController(IUserBl userBl)
+        {
+            _userBl = userBl;
+        }
+
+        [HttpPost("SignUp/Doctor")]
+      // [Authorize(Roles = "Doctor")]
+        public async Task<IActionResult> RegisterDoctor([FromBody] UserEntity userDto)
+        {
+            try
+            {
+                userDto.UserRole = "doctor";
+                await _userBl.Register(userDto);
+                return Ok("Doctor registered successfully");
+            }
+            catch (ArgumentException argEx)
+            {
+                return BadRequest(argEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while registering the doctor: {ex}");
+                return StatusCode(500, "An error occurred while registering the doctor. Please try again later or contact support.");
+            }
+        }
+
+        [HttpPost("SignUp/Admin")]
+      //  [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] UserEntity userDto)
+        {
+            try
+            {
+                userDto.UserRole = "admin";
+                await _userBl.Register(userDto);
+                return Ok("Admin registered successfully");
+            }
+            catch (ArgumentException argEx)
+            {
+                return BadRequest(argEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while registering the admin: {ex}");
+                return StatusCode(500, "An error occurred while registering the admin. Please try again later or contact support.");
+            }
+        }
+
+        [HttpPost("SignUp/Patient")]
+       // [Authorize(Roles = "Patient")]
+        public async Task<IActionResult> RegisterPatient([FromBody] UserEntity userDto)
+        {
+            try
+            {
+                userDto.UserRole = "patient";
+                await _userBl.Register(userDto);
+                return Ok("Patient registered successfully");
+            }
+            catch (ArgumentException argEx)
+            {
+                return BadRequest(argEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while registering the patient: {ex}");
+                return StatusCode(500, "An error occurred while registering the patient. Please try again later or contact support.");
+            }
+        }
+
+        [HttpGet("AllUsers")]
+        // [Authorize]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            try
+            {
+                var users = await _userBl.GetAllUsers();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while retrieving all users: {ex}");
+                return StatusCode(500, "An error occurred while retrieving all users. Please try again later or contact support.");
+            }
+        }
+
+        //login
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginEntity userLoginEntity)
+        {
+            try
+            {
+                var users = await _userBl.Login(userLoginEntity);
+                if (users.Any())
+                {
+                    return Ok("Login successful");
+                }
+                else
+                {
+                    return BadRequest("Invalid email or password. Please check your credentials and try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while logging in: {ex}");
+                return StatusCode(500, "An error occurred while logging in. Please try again later or contact support.");
+            }
+        }
+
+        //getbyid
+        [HttpGet("GetUserByUserId")]
+        // [Authorize]
+        public async Task<IActionResult> GetUserByUserId(int userid)
+        {
+            try
+            {
+                var user = await _userBl.GetUserByUserId(userid);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while retrieving user with ID {userid}. Please try again later or contact support.");
+            }
+        }
+
+        [HttpGet("AllUserDoctors")]
+        //[Authorize]
+        public async Task<IActionResult> GetAllUserDoctors()
+        {
+            try
+            {
+                var doctors = await _userBl.GetAllUserDoctor();
+                return Ok(doctors);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while retrieving all doctors. Please try again later or contact support.");
+            }
+        }
+
+
+    }
+}
---
+++ UserManagementService/Program.cs
@@ -0,0 +1,181 @@
+
+/*using BusinessLayer.Interface;
+using BusinessLayer.Services;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RepositoryLayer.Context;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register services
+builder.Services.AddSingleton<UserDapperContext>();
+builder.Services.AddScoped<IUser, UserService>();
+builder.Services.AddScoped<IUserBl, UserServiceBl>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = true;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = key,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
+*/
+
+using BusinessLayer.Interface;
+using BusinessLayer.Services;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RepositoryLayer.Context;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register services
+builder.Services.AddSingleton<UserDapperContext>();
+builder.Services.AddScoped<IUser, UserService>();
+builder.Services.AddScoped<IUserBl, UserServiceBl>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Get USerNotes based on ID", Version = "v1" });
+
+    // Define the JWT bearer scheme
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "JWT Authorization header using the Bearer scheme",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = JwtBearerDefaults.AuthenticationScheme
+        }
+    };
+
+    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+
+    // Require JWT tokens to be passed on requests
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            securityScheme,
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddDistributedMemoryCache();
+
+//jwt
+
+// Add JWT authentication
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]));
+//var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true; // Set to true in production
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        ValidateLifetime = true,
+
+
+
+        ClockSkew = TimeSpan.Zero,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = key
+    };
+});
+
+
+//Ending...
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(policy =>
+    {
+        policy.WithOrigins("http://localhost:7254", "https://localhost:7254")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithHeaders(HeaderNames.ContentType);
+    });
+}
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
