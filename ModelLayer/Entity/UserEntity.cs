a073a481dc2092fdfdf4ca9da9d976916851e1de
poojakashyap-2920 <pooja337026@gmail.com>
2024-04-18 18:45:30 +0530

    usermanagementse  modelLayer added

---
+++ ModelLayer/Entity/UserEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Entity
+{
+   public class UserEntity
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string UserRole { get; set; }
+    }
+}
