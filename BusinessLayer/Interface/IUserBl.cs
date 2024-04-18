a65e352fcfdbb874377ff9eaa85af9e038b7f282
poojakashyap-2920 <pooja337026@gmail.com>
2024-04-18 18:41:46 +0530

    usermanagementse bussinesslayer added

---
+++ BusinessLayer/Interface/IUserBl.cs
@@ -0,0 +1,25 @@
+﻿using ModelLayer.Entity;
+using RepositoryLayer.Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface IUserBl
+    {
+        // Sign up
+        Task<string> Register(UserEntity userEntity);
+
+        // Get all users
+        Task<List<UserEntity>> GetAllUsers();
+
+        // Login
+        Task<IEnumerable<UserEntity>> Login(UserLoginEntity userLoginEntity);
+
+        // Get user by ID
+        Task<IEnumerable<UserEntity>> GetUserByUserId(int userId);
+
+        // Get all user doctors
+        Task<List<DoctorInfo>> GetAllUserDoctor();
+    }
+}
---
+++ BusinessLayer/Services/UserServiceBl.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Interface;
+using ModelLayer.Entity;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class UserServiceBl : IUserBl
+    {
+        private readonly IUser _user;
+
+        public UserServiceBl(IUser user)
+        {
+            _user = user;
+        }
+
+
+        public Task<string> Register(UserEntity userEntity)
+        {
+
+            return _user.Register(userEntity);
+        }
+
+        //getall
+        public async Task<List<UserEntity>> GetAllUsers()
+        {
+            return await _user.GetAllUsers();
+        }
+
+        public string EncyptionPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        //login
+        public async Task<IEnumerable<UserEntity>> Login(UserLoginEntity userLoginEntity)
+        {
+           
+            return await _user.Login(userLoginEntity);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetUserByUserId(int userid)
+        {
+            return await _user.GetUserByUserId(userid);
+        }
+
+        public async Task<List<DoctorInfo>> GetAllUserDoctor()
+        { 
+            return await _user.GetAllUserDoctor
+                ();
+        }
+    }
+}
